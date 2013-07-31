8b6164a72b01f4e1325baa8a22ddb68f08ea79a5
enbake <enbake@enbake.(none)>
2013-07-31 14:21:34 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wisewords::Application.config.secret_key_base = '6a38fb1d8307c3b087970951595ce6738b9556f3d2c68873e63f028148263da7dbafdec02252b4ca6d61dceb94b1446ba2d1b7f80e837990ebc73945ac6bfb92'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wisewords::Application.config.session_store :cookie_store, key: '_wisewords_session'
